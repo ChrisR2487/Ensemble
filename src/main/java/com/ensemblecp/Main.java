39d5139d7069d8711bd397dd8bfca92397fd0d10
KodyBackenstoes <kodybackenstoes@gmail.com>
2022-02-20 17:29:49 -0500

    Added Project Creator Screen

--- src/main/java/com/ensemblecp/Main.java
+++ src/main/java/com/ensemblecp/Main.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("projCreator.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- target/classes/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.ensemblecp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.ensemblecp to javafx.fxml;
-    exports com.ensemblecp;
-}
