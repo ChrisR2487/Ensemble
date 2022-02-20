e0fa6cb2896853787adfee0d3a8bd31e70278372
Ryan Ward <rdw5286@psu.edu>
2022-02-20 12:38:13 -0500

    initial commit

---
+++ src/main/java/com/ensemblecp/Component.java
@@ -0,0 +1,9 @@
+package com.ensemblecp;
+
+// Component Class
+/**
+ * A class used to temporarily create a java object
+ * which represents the data of a component.
+ */
+public class Component {
+}
