452d3328358a9117b0cb1d32dc5ee37b07bf351e
Ryan Ward <rdw5286@psu.edu>
2022-02-20 12:38:13 -0500

    initial commit

---
+++ src/main/java/com/ensemblecp/Main.java
@@ -0,0 +1,20 @@
+package com.ensemblecp;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
