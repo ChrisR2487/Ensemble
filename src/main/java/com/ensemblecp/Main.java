8f10f18813337795e62be805cd08dfccd2896a33
Christopher Roberts <chrisisaninja@hotmail.com>
2022-02-20 21:59:27 -0500

    Created Dashboard Gui with buttons

--- src/main/java/com/ensemblecp/Main.java
+++ src/main/java/com/ensemblecp/Main.java
@@ -1,4 +1,5 @@
 package com.ensemblecp;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,8 +8,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
