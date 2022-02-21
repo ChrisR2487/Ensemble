6c0b9bbf1a858d38d2a3ac4a13189c00472b6781
Ryan Ward <rdw5286@psu.edu>
2022-02-21 13:18:09 -0500

    Update ryan branch

--- src/main/java/com/ensemblecp/Main.java
+++ src/main/java/com/ensemblecp/Main.java
@@ -3,15 +3,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class Main extends Application {
+    private final boolean isFullscreen = false;
+    private FXMLLoader fxmlLoader;
+    private Scene mainScene;
+    private Stage mainStage;
+
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("projCreator.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException {
+        // Initialize stage
+        mainStage = stage;
+        mainStage.setFullScreen(isFullscreen);
+        mainStage.setTitle("Ensemble");
+            //stage.initStyle(StageStyle.UNDECORATED); TODO: change to login screen, then use this
+
+        // Show startup screen
+        show("projCreator");
+        mainStage.show();
+    }
+
+    public void show(String screenName) throws IOException {
+        fxmlLoader = new FXMLLoader(Main.class.getResource(screenName+".fxml"));
+        Scene newScene = new Scene(fxmlLoader.load(), 1600, 900);
+        mainStage.setScene(newScene);
     }
 
     public static void main(String[] args) {
