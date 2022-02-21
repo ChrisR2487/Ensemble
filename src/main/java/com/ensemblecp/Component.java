6f11092037a0609c0c60c55858083448bdaa0e13
kodyb <kodybackenstoes@gmail.com>
2022-02-21 16:51:44 -0500

    added files

--- src/main/java/com/ensemblecp/Component.java
+++
@@ -1,9 +0,0 @@
-package com.ensemblecp;
-
-// Component Class
-/**
- * A class used to temporarily create a java object
- * which represents the data of a component.
- */
-public class Component {
-}
