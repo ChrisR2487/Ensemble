76cf1ee19ebce5f8696c1de23fd5b285786bd863
szf59 <sabr.fathima@gmail.com>
2022-03-21 16:13:26 -0400

    Create issue

--- src/main/java/com/ensemblecp/CreateIssueController.java
+++ src/main/java/com/ensemblecp/CreateIssueController.java
@@ -10,7 +10,70 @@ public class CreateIssueController {
     }
 
     public void submitIssue_onClick(ActionEvent actionEvent) throws IOException {
-        // TODO: Create issue in system
+        // Get issue info
+        Database db = new Database();
+        HashMap<String, String> info = new HashMap<>();
+        info.put("message", messageField.getText()); // TODO: Change to correct name of message field
+
+        // Determine type
+        String getSelectedField = null;
+
+        ToggleGroup group = new ToggleGroup();
+
+        ToggleButton tb1 = new ToggleButton("No Score");
+        tb1.setToggleGroup(group);
+
+        ToggleButton tb2 = new ToggleButton("Timeline");
+        tb2.setToggleGroup(group);
+
+        ToggleButton tb3 = new ToggleButton("Budget");
+        tb3.setToggleGroup(group);
+
+        ToggleButton tb4 = new ToggleButton("Task");
+        tb4.setToggleGroup(group);
+
+        ToggleButton tb5 = new ToggleButton("Team");
+        tb5.setToggleGroup(group);
+
+        if(tb1.isSelected()){
+            getSelectedField = "No Score";
+        }
+        if(tb2.isSelected()){
+            getSelectedField = "Timeline";
+        }
+        if(tb3.isSelected()){
+            getSelectedField = "Budget";
+        }
+        if(tb4.isSelected()){
+            getSelectedField = "Task";
+        }
+        if(tb5.isSelected()){
+            getSelectedField = "Team";
+        }
+
+        int type = -1;
+        switch (getSelectedField) {
+            case "No Score": type = IssueType.NO_SCORE; break;
+            case "Timeline": type = IssueType.TIMELINE; break;
+            case "Budget": type = IssueType.BUDGET; break;
+            case "Task": type = IssueType.TASK; break;
+            case "Team": type = IssueType.TEAM; break;
+        }
+        info.put("type", String.valueOf(type));
+
+        // Create issue record
+        db.createIssue(info);
+
+        // Update issue score
+        float score = -1.0f; // Score to add to project
+        switch (getSelectedField) {
+            case "No Score": score = IssueScore.NO_SCORE; break;
+            case "Timeline": score = IssueScore.ISSUE_TIMELINE; break;
+            case "Budget": score = IssueScore.ISSUE_BUDGET; break;
+            case "Task": score = IssueScore.ISSUE_TASK; break;
+            case "Team": score = IssueScore.ISSUE_TEAM; break;
+        }
+        db.updateIssueScore(Main.curProject.getPid(), score);
 
         // Go back to issue list
         Main.show("projIssues");
