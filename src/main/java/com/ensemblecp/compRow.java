078a1317bec459a0bb52e3225e42ed54bf737fa6
Christopher Roberts <chrisisaninja@hotmail.com>
2022-03-25 12:55:59 -0400

    added custom comp controller functions.

---
+++ src/main/java/com/ensemblecp/compRow.java
@@ -0,0 +1,33 @@
+package com.ensemblecp;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.CheckBox;
+
+public class compRow {
+
+    private StringProperty type;
+    private StringProperty textField;
+
+    //Methods
+    public StringProperty typeProperty() {
+        if (type == null) type = new SimpleStringProperty(this, "type");
+        return type;
+    }
+    public StringProperty textProperty() {
+        if (textField == null) textField = new SimpleStringProperty(this, "text");
+        return textField;
+    }
+
+    //Setters
+    public void setType(String type) { typeProperty().set(type); }
+    public void setTextField(String textField) {textProperty().set(textField); }
+
+    //Getters
+    public String getType() { return typeProperty().get(); }
+    public String getTextField() { return textProperty().get(); }
+
+
+
+
+}
--- src/main/java/com/ensemblecp/customCompController.java
+++ src/main/java/com/ensemblecp/customCompController.java
@@ -3,10 +3,9 @@ package com.ensemblecp;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -16,8 +15,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.ResourceBundle;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class customCompController implements Initializable {
 
@@ -28,13 +27,16 @@ public class customCompController implements Initializable {
     @FXML public Button submit_Button;
     @FXML public Button addItem;
     @FXML public Button exitButton;
-    @FXML public StackPane stackPane;
     @FXML public TextField inputField;
     @FXML public MenuButton menuButton;
     @FXML public VBox mainVBox;
     @FXML public VBox fieldVBox;
     @FXML public TextField compTitle;
     @FXML public Button cancelButton;
+    String template = "";
+    ArrayList<compRow> groupList = new ArrayList<>();
+    compRow gr = new compRow();
+    int partID = 0;
 
 
     @Override
@@ -71,53 +73,98 @@ public class customCompController implements Initializable {
         intItem.setOnAction(e -> {
             menuButton.setText(intItem.getText());
         });
-        tableItem.setOnAction(e -> {
-            menuButton.setText(tableItem.getText());
-        });
-        fileItem.setOnAction(e -> {
-            menuButton.setText(fileItem.getText());
-        });
-    }
-
-    public void exitButton_onClick(MouseEvent mouseEvent) {
-        System.exit(-1);
     }
 
     public void addField_onClick(Event actionEvent) {
         MenuItem item1 = new MenuItem("Integer");
         MenuItem item2 = new MenuItem("String");
-        MenuItem item3 = new MenuItem("Table");
-        MenuItem item4 = new MenuItem("File");
-        MenuButton m = new MenuButton("Select Type",null, item1,item2,item3,item4);
+        //MenuItem item3 = new MenuItem("Table");
+        //MenuItem item4 = new MenuItem("File");
+        MenuButton m = new MenuButton("Select Type",null, item1,item2);
+        TextField t = new TextField();
 
+        //MenuButton Listeners
         item1.setOnAction(e -> {
             m.setText(item1.getText());
+            gr.setType(m.getText());
+            template = m.getText().substring(0,1).toUpperCase();
         });
         item2.setOnAction(e -> {
             m.setText(item2.getText());
+            gr.setType(m.getText());
         });
+        /*
         item3.setOnAction(e -> {
             m.setText(item3.getText());
+
         });
         item4.setOnAction(e -> {
             m.setText(item4.getText());
+
         });
 
-        TextField t = new TextField();
+         */
+
         m.setPrefWidth(160);
         m.setPrefHeight(40);
         t.setPrefWidth(600);
         t.setPrefHeight(40);
-        HBox newBox = new HBox(m,t);
 
-        fieldVBox.setSpacing(20);
+        HBox newBox = new HBox(m,t);
+        fieldVBox.setSpacing(10);
         fieldVBox.getChildren().add(newBox);
+    }
+    public void removeField_onClick(Event actionEvent) {
+        ArrayList<Node> VBox = getAllNodes(fieldVBox);
+        VBox.remove(VBox.size()-1);
+        /*
+        for (Node parent : VBox){
+            if (parent instanceof HBox){
+
+            }
+        }
+         */
     }
 
     public void submit_Button_onClick(Event actionEvent) throws SQLException, IOException {
         //partId shows the order, use layout of components
         //add new record to Component table when creating a template
+        String temp = "";
+        String template = "";
+        //Get Template
 
+        ArrayList<Node> VBox = getAllNodes(fieldVBox);
+
+        for (Node parent : VBox){
+            if (parent instanceof HBox){
+                for (Node children : ((HBox) parent).getChildren()) {
+                    if(children instanceof MenuButton){
+                        if(Objects.equals(((MenuButton) children).getText(), "String")){
+                            template += "S";
+                            children.setId(temp + partID++);
+                            System.out.println("String button found");
+                            System.out.println(children.getId());
+                        }
+                        if(Objects.equals(((MenuButton) children).getText(), "Integer")){
+                            template += "I";
+                            children.setId(temp + partID++);
+                            System.out.println("Integer button found");
+                            System.out.println(children.getId());
+                        }
+                    }
+                    if(children instanceof TextField){
+                        //gr.setTextField(((TextField) children).getText());
+
+                        System.out.println(((TextField) children).getText());
+                    }
+                }
+            }
+        }
+        groupList.add(gr);
+        System.out.println(template);
+        //System.out.println(groupList);
+         /*
+        //Store Input in Hashmap
         HashMap<String, String> info = new HashMap<String, String>();
         info.put("cid", "1");
         info.put("title", compTitle.getText());
@@ -128,11 +175,31 @@ public class customCompController implements Initializable {
         ResultSet rs = db.createComponent(info);
 
         db.closeDB();
-        Main.show("projOverview");
+
+         */
+        //Main.show("projOverview");
+    }
+
+    public static ArrayList<Node> getAllNodes(Parent root) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        addAllDescendents(root, nodes);
+        return nodes;
+    }
+
+    private static void addAllDescendents(Parent parent, ArrayList<Node> nodes) {
+        for (Node node : parent.getChildrenUnmodifiable()) {
+            nodes.add(node);
+            if (node instanceof Parent)
+                addAllDescendents((Parent)node, nodes);
+        }
     }
 
     public void cancelButton_onClick(Event actionEvent) throws IOException {
         Main.show("compCreator");
+    }
+
+    public void exitButton_onClick(MouseEvent mouseEvent) {
+        System.exit(-1);
     }
 
 
